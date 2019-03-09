93365139b42f20dc054d62b9756d175e7891c77f
obskyr <powpowd@gmail.com>
2019-03-09 07:30:31 +0100

    Add a CLI for half the commands.

--- software/framune.py
+++ software/framune.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import argparse
+import json
 import os
 import struct
 import sys
@@ -51,6 +53,9 @@ class Framune(object):
                                               inter_byte_timeout=MIN_TIMEOUT)
         self._chip = MemoryChip(None, None, None, None, framune=self)
     
+    def __enter__(self):
+        return self
+
     def __exit__(self, *args):
         self.close()
 
@@ -247,14 +252,153 @@ class MemoryChip(object):
         ))
 
 def main(*argv):
-    script_name = os.path.split(__file__)[-1]
-    try:
-        serial_port = argv[0]
-    except IndexError:
-        print("Usage: {} <serial port>", file=sys.stderr)
+    script_name = os.path.basename(__file__)
+
+    # Tiny details!
+    class KindArgumentParser(argparse.ArgumentParser):
+        def error(self, message):
+            print(self.usage % {'prog': self.prog}, file=sys.stderr, end="\n\n")
+            print(self.description % {'prog': self.prog}, file=sys.stderr, end="\n\n")
+            print("Run \"{} --help\" for more detailed information!".format(self.prog), file=sys.stderr)
+            sys.exit(1)
+
+    # More tiny details!
+    class ProperHelpFormatter(argparse.RawTextHelpFormatter):
+        def add_usage(self, usage, actions, groups, prefix=None):
+            if prefix is None:
+                prefix = 'Usage: '
+            return super(ProperHelpFormatter, self).add_usage(usage, actions, groups, prefix)
+
+    parser = KindArgumentParser(
+        prog=script_name,
+        usage="%(prog)s [-h] [--analyze] [--no-version-check] <port> "
+              "<version|analyze|read|write> ...",
+        description="Interface with an F-Ramune (memory chip programmer and tester).\n\n"
+        "Examples:\n"
+        "%(prog)s COM5 analyze\n"
+        "%(prog)s /dev/ttyS2 read -a 0x1000 -s 0x100 -o data.hex\n"
+        "%(prog)s /dev/tty.usbserial-A6004byf write -i data.hex",
+        formatter_class=ProperHelpFormatter,
+        add_help=False
+    )
+        
+    try: # Even more tiny details!
+        parser._positionals.title = "Positional arguments"
+        parser._optionals.title = "Optional arguments"
+    except AttributeError:
+        pass
+
+    parser.add_argument(
+        'port',
+        help="The serial port your F-Ramune is connected to."
+    )
+    parser.add_argument(
+        'command', metavar='command',
+        help="What to do. Valid commands are: \"version\", \"analyze\", \"read\", and \"write\".",
+        choices=('version', 'analyze', 'read', 'write')
+    )
     
-    with Framune(serial_port) as framune:
-        print("Connected F-Ramune protocol version: {}".format(framune.get_version))
+    parser.add_argument(
+        '-h', '--help',
+        action='help', default=argparse.SUPPRESS,
+        help="Show this help and exit."
+    )
+    parser.add_argument(
+        '--analyze',
+        action='store_true',
+        help="Before running the command, analyze the chip to set the correct properties."
+    )
+    parser.add_argument(
+        '--no-version-check',
+        action='store_true',
+        help="Skip verifying that the script's and the F-Ramune's protocol versions match."
+    )
+    parser.add_argument(
+        '-a', '--address', metavar='address', type=int, default=0,
+        help="Used with the \"read\" and \"write\" commands. The address to start at."
+             "Defaults to 0."
+    )
+    parser.add_argument(
+        '-s', '--size', metavar='size', type=int, default=None,
+        help="Used with the \"read\" and \"write\" commands. The number of bytes."
+             "Required for reading if not using --analyze."
+    )
+    parser.add_argument(
+        '-i', metavar='path',
+        help="Used with the \"write\" command. The file to get the data to write from.\n"
+             "By omitting this and piping input, the data can be gotten from stdin."
+    )
+    parser.add_argument(
+        '-o', metavar='path',
+        help="Used with the \"read\" command. The file to save the read data to.\n"
+             "By omitting this and piping output, the data can be output to stdout."
+    )
+    parser.add_argument(
+        '-j', '--json', action='store_true',
+        help="Used with the \"analyze\" command. Outputs the chip information in JSON form."
+    )
+
+    arguments = parser.parse_args(argv)
+
+    if arguments.command == 'read' and arguments.o is None and not sys.stdout.isatty():
+        print("No output specified! Please either specify -o or pipe output.",
+              file=sys.stderr)
+        return 1
+    if arguments.command == 'write' and arguments.i is None and not sys.stdin.isatty():
+        print("No input specified! Please either specify -i or pipe input.",
+              file=sys.stderr)
+        return 1
+
+    with Framune(arguments.port) as framune:
+        if not arguments.no_version_check and arguments.command != 'version':
+            version = framune.get_version()
+            if version < PROTOCOL_VERSION:
+                print("The connected F-Ramune is running outdated software! "
+                      "Please update it.",
+                      file=sys.stderr)
+                return 1
+            elif version > PROTOCOL_VERSION:
+                print("The connected F-Ramune is running a newer protocol version "
+                      "than this program!\n"
+                      "Please update {}.".format(script_name),
+                      file=sys.stderr)
+                return 1
+        
+        if arguments.analyze and not arguments.analyze:
+            framune.analyze()
+        
+        if arguments.command == 'version':
+            print(framune.get_version())
+        elif arguments.command == 'analyze':
+            framune.analyze()
+            if arguments.json:
+                properties = OrderedDict(
+                    (k, getattr(framune.chip, k))
+                    for k in MEMORY_CHIP_DATA_STRUCTURE
+                )
+                print(json.dumps(properties, indent=4))
+            else:
+                yn = lambda x: "Yes" if x else "No"
+                def format_size(size):
+                    n = size
+                    for unit in ("", "KiB", "MiB"):
+                        if n < 1024:
+                            break
+                        n /= 1024
+                    else:
+                        unit = "GiB"
+                    n = "{}".format(round(n)) if n - int(n) < 0.00001 else "~{:.1f}".format(n)
+                    return "{} ({} {})".format(size, n, unit)
+                rows = (
+                    ('is_operational', "Is operational:  ", yn),
+                    ('size',           "Size:            ", format_size),
+                    ('is_nonvolatile', "Is non-volatile: ", yn),
+                    ('is_eeprom',      "Is EEPROM:       ", yn)
+                )
+                for attr, label, transformer in rows:
+                    attr = getattr(framune.chip, attr)
+                    value = transformer(attr) if attr is not None else "Unknown"
+                    print("{}{}".format(label, value))
 
     return 0
 
